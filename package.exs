28e0544ba1510aef684d035b23e63f8895e6d07f
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2014-03-03 21:53:50 -0700

    Added another label.

--- package.exs
+++ package.exs
@@ -2,7 +2,7 @@ Expm.Package.new(
     name: "exemplar",
     description: "HTML as S-Expressions for LFE",
     version: "0.0.1",
-    keywords: ["LFE", "Lisp", "Library", "HTML", "YAWS"],
+    keywords: ["LFE", "Lisp", "Library", "HTML", "YAWS", "Web"],
     maintainers: [[name: "Duncan McGreggor",
                    email: "oubiwann@cogitat.io"]],
     repositories: [[github: "lfe/exemplar"]])
