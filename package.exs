5a4c1d0479e79c639ff56918308ae91ce69aaac5
Duncan McGreggor <duncan@cogitat.io>
2014-03-20 11:18:00 -0500

    Bumped version.

--- package.exs
+++ package.exs
@@ -1,7 +1,7 @@
 Expm.Package.new(
     name: "exemplar",
     description: "HTML as S-Expressions for LFE",
-    version: "0.0.2",
+    version: "0.0.3",
     keywords: ["LFE", "Lisp", "Library", "HTML", "YAWS", "Web"],
     maintainers: [[name: "Duncan McGreggor",
                    email: "oubiwann@cogitat.io"]],
